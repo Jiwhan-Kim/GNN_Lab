91f27a6305aa193f8fdbfbd966ef8336bd0d029c
Jiwhan-Kim <jiwhan1015@yonsei.ac.kr>
2024-01-13 12:13:07 +0900

    24.01.13. First Commit

---
+++ main.py
@@ -0,0 +1,20 @@
+'''
+GNN Lab
+Main File
+Jiwhan Kim (Yonsei Univ. SSE21)
+'''
+
+# Modules
+import torch
+import numpy as np
+import sys
+
+# Models
+
+# Trainers
+
+# Constants
+
+if __name__ == "__main__":
+    print("Hello World")
+        
